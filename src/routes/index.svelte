23d4c8e42fee40640bb425ea59d5f7f8e84ce9ac
Torrance Graham <torrance.m.graham@gmail.com>
2020-06-17 17:43:52 -0400

    new setup

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<h1>Hello Index!</h1>
+<style lang="scss">
+$background-color: #fff9ea;
+
+h1 {
+    background-color: $background-color;
+}
+</style>
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import Koa from "koa";
+import connect from "koa-connect";
+import koaStatic from "koa-static";
+import * as sapper from '@sapper/server';
+
+const { PORT } = process.env;
+
+const app = new Koa();
+
+app.use(koaStatic('/public'));
+
+app.use(connect((req, res, next) => {
+  req.originalUrl = req.url;
+  sapper.middleware()(req, res, next);
+}));
+
+app.listen(PORT);
