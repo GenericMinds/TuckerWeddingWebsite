82f6f79268912cc1233667dac51358f8f0ae7f4c
Bri Long <briana.m.long@outlook.com>
2020-06-23 16:22:27 -0400

    remove path stuff

--- src/routes/_components/MenuIcon.svelte
+++ src/routes/_components/MenuIcon.svelte
@@ -32,8 +32,8 @@
   on:toggle={handleToggle}
   zIndex={100}
   bind:isOpen={isOpen}>    
-  
-    <svg slot="parent" on:click={() => handleToggle()} x="0px" y="0px" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve" height="40px" width="40px">
+    
+    <svg slot="parent" on:click="{() => handleToggle()}" x="0px" y="0px" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve" height="40px" width="40px">
           <path fill="#ddd499" d="M8.667,15h30c0.552,0,1-0.447,1-1s-0.448-1-1-1h-30c-0.552,0-1,0.447-1,1S8.114,15,8.667,15z"/>
           <path fill="#ddd499" d="M8.667,37h30c0.552,0,1-0.447,1-1s-0.448-1-1-1h-30c-0.552,0-1,0.447-1,1S8.114,37,8.667,37z"/>
           <path fill="#ddd499" d="M8.667,26h30c0.552,0,1-0.447,1-1s-0.448-1-1-1h-30c-0.552,0-1,0.447-1,1S8.114,26,8.667,26z"/>
