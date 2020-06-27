3388700f04370f0c5a144bcc56d34cddf881a957
Bri Long <briana.m.long@outlook.com>
2020-06-27 00:57:10 -0400

    added test svg

--- __sapper__/dev/client/client.a7708bff.js
+++
@@ -1,2 +0,0 @@
-import './client.07a81c40.js';
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2xpZW50LmE3NzA4YmZmLmpzIiwic291cmNlcyI6W10sInNvdXJjZXNDb250ZW50IjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
--- src/routes/_components/MenuIcon.svelte
+++ src/routes/_components/MenuIcon.svelte
@@ -18,11 +18,7 @@
 </script>
 
 <div>
-    <svg on:click={handleToggle} x='0px' y='0px' viewBox='0 0 50 50' enable-background='new 0 0 50 50' xml:space='preserve' height='40px' width='40px'>
-        <path fill='#ddd499' d='M8.667,15h30c0.552,0,1-0.447,1-1s-0.448-1-1-1h-30c-0.552,0-1,0.447-1,1S8.114,15,8.667,15z'/>
-        <path fill='#ddd499' d='M8.667,37h30c0.552,0,1-0.447,1-1s-0.448-1-1-1h-30c-0.552,0-1,0.447-1,1S8.114,37,8.667,37z'/>
-        <path fill='#ddd499' d='M8.667,26h30c0.552,0,1-0.447,1-1s-0.448-1-1-1h-30c-0.552,0-1,0.447-1,1S8.114,26,8.667,26z'/>
-    </svg>
+    <img alt="daBurger" src="./Hamburger_icon.svg" />
 </div>
 
 <!-- <Overlay on:toggle={handleToggle} zIndex={100} bind:isOpen={isOpen}>    
@@ -68,13 +64,13 @@
         padding-top: 3vh;
     }
 
-    svg {
+    img {
         position: fixed;
         right: 0;
         top: 0;
     }
 
-    svg:hover, li:hover {
+    img:hover, li:hover {
         cursor: pointer;
     }
 </style>
